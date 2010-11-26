e1f12ffe1db6880ca23ee56805cf18dee2a11934
unknown <BOS CONTROL@.(none)>
2010-11-26 15:52:37 -0800

    sdfasd

--- ReadMACAddress/Form2.vb
+++ ReadMACAddress/Form2.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form2
 
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
         Form1.Show()
     End Sub
 End Class
