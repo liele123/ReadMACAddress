3675455ce59b462e3aa2f738d8b6c2051f91a819
unknown <BOS CONTROL@.(none)>
2010-11-26 13:00:10 -0800

    Add Exit
    Signed-off-by: unknown <BOS CONTROL@.(none)>

--- ReadMACAddress/Form1.Designer.vb
+++ ReadMACAddress/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.txtDisplay = New System.Windows.Forms.TextBox
         Me.btnReadMac = New System.Windows.Forms.Button
         Me.Label1 = New System.Windows.Forms.Label
+        Me.Button1 = New System.Windows.Forms.Button
         Me.SuspendLayout()
         '
         'txtDisplay
@@ -52,11 +53,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Display"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(98, 227)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Exit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnReadMac)
         Me.Controls.Add(Me.txtDisplay)
@@ -69,5 +80,6 @@ Partial Class Form1
     Friend WithEvents txtDisplay As System.Windows.Forms.TextBox
     Friend WithEvents btnReadMac As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- ReadMACAddress/Form1.vb
+++ ReadMACAddress/Form1.vb
@@ -45,4 +45,8 @@ Public Class Form1
             Return ""
         End Try
     End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
 End Class
