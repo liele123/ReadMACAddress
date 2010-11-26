c46632cf7e7e168b58d39ca66c08446b0a22a6c5
unknown <BOS CONTROL@.(none)>
2010-11-26 13:39:28 -0800

    added label

--- ReadMACAddress/Form1.Designer.vb
+++ ReadMACAddress/Form1.Designer.vb
@@ -26,6 +26,7 @@ Partial Class Form1
         Me.btnReadMac = New System.Windows.Forms.Button
         Me.Label1 = New System.Windows.Forms.Label
         Me.Button1 = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
         Me.SuspendLayout()
         '
         'txtDisplay
@@ -62,11 +63,21 @@ Partial Class Form1
         Me.Button1.Text = "Exit"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(118, 34)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.btnReadMac)
@@ -81,5 +92,6 @@ Partial Class Form1
     Friend WithEvents btnReadMac As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
